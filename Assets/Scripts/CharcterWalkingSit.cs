270cbbbabf25263445509330bc36023edcb0652c
zah tayar <zahtayar1993@gmail.com>
2021-05-07 18:35:15 +0300

    Initial commit

---
+++ Assets/Scripts/CharcterWalkingSit.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class CharcterWalkingSit : MonoBehaviour
+{
+    private Animator anim;
+    public GameObject target;
+    private NavMeshAgent agent;
+    
+   // public GameObject sofa;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+        anim.SetBool("walkin", true);
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        other = agent.GetComponent<Collider>();
+        Collider other1 = target.GetComponent<Collider>();
+        if (other.Equals( other1))
+        {
+            anim.SetBool("walkin", false);
+            anim.SetBool("siting", true);
+        }
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        //if (anim.GetBool("walkin"))
+        agent.SetDestination(target.transform.position);
+        /*
+        Collider other1 = target.GetComponent<Collider>();
+        if (other == other1)
+        {
+            anim.SetBool("walkin", false);
+            anim.SetBool("siting", true);
+        }
+        */
+        /*
+        RaycastHit hit;
+        if(Physics.Raycast(agent.transform.position,agent.transform.forward,out hit))
+        {
+            if (hit.transform.gameObject.name==target.transform.gameObject.name)
+            {
+                anim.SetBool("walkin", false);
+                anim.SetBool("siting", true);
+            }
+            else
+            {
+                anim.SetBool("siting", false);
+                anim.SetBool("walkin", true);
+            }
+        }
+        */
+        
+
+
+
+    }
+}
